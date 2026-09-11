83753ac05c3b972d168cfe53245b84e775eebf0d
Hussain Ahmed <hussainahmed.vu@gmail.com>
2026-09-11 22:10:03 +0600

    chore: update next.config.ts and pnpm-workspace.yaml

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  images: {
-    unoptimized: true,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-        ],
-      },
-      {
-        source: "/_next/static/:path*",
-        headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Cross-Origin-Resource-Policy",
-            value: "cross-origin",
-          },
-        ],
-      },
-    ];
-  },
+	reactCompiler: true,
+	images: {
+		unoptimized: true,
+	},
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Cross-Origin-Opener-Policy",
+						value: "same-origin",
+					},
+					{
+						key: "Cross-Origin-Embedder-Policy",
+						value: "require-corp",
+					},
+				],
+			},
+			{
+				source: "/_next/static/:path*",
+				headers: [
+					{
+						key: "Cross-Origin-Embedder-Policy",
+						value: "require-corp",
+					},
+					{
+						key: "Cross-Origin-Resource-Policy",
+						value: "cross-origin",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
